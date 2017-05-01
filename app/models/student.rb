0bfd68f70e2605e03711ae385abf3adba217f532
gabystern <stern.gabrielle@gmail.com>
2017-04-30 20:07:34 -0400

    3 active/inactive student errors

--- app/models/student.rb
+++ app/models/student.rb
@@ -1,5 +1,27 @@
 class Student < ActiveRecord::Base
+
   def to_s
     self.first_name + " " + self.last_name
   end
+
+  def active_message
+     if self.active
+       message = "This student is currently active."
+     else
+       message = "This student is currently inactive."
+     end
+     message
+  end
+
+
+  def activate_student_path
+     if self.active == false
+       self.active = true
+     elsif self.active == true
+       self.active = true
+     end
+
+   end
+
+
 end
